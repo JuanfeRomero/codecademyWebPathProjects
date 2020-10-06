f3a2d12d18cd2c608e81bb0b0dd303d53bd9fb9a
JuanfeRomero <juanferomerocode@gmail.com>
2020-10-06 04:29:26 -0300

    added and tested inRange method

--- remakingLodash/_.js
+++ remakingLodash/_.js
@@ -2,15 +2,23 @@ const _ = {};
 _.clamp = (number, lowerBound, upperBound) =>{
     if(number < lowerBound){
         return lowerBound;
-    }else if(number > upperBound){
-        return upperBound;
     }else{
-        return number;
+        return number > upperBound? upperBound: number;
     }
 };
-
-
-
+_.inRange = (num, start, end) =>{
+    tempValue = 0;
+    if(typeof end === 'undefined'){
+      end = start;
+      start = tempValue;
+    }
+    if(start > end){
+      tempValue = start;
+      start = end;
+      end  = tempValue;
+    }
+    return num >= start && num < end;
+}
 
 console.log('la funcion _ esta compuesta de la siguiente forma: \n');
 console.log(_); 
