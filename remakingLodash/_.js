fe8646688b75cc02a3d2cbb0d1ae6ecac2cf99fd
JuanfeRomero <juanferomerocode@gmail.com>
2020-10-08 01:35:21 -0300

    finish all the methods of lodash library prompted in the exercise

--- remakingLodash/_.js
+++ remakingLodash/_.js
@@ -6,6 +6,7 @@ _.clamp = (number, lowerBound, upperBound) =>{
         return number > upperBound? upperBound: number;
     }
 };
+
 _.inRange = (num, start, end) =>{
     tempValue = 0;
     if(typeof end === 'undefined'){
@@ -20,7 +21,77 @@ _.inRange = (num, start, end) =>{
     return num >= start && num < end;
 }
 
-console.log('la funcion _ esta compuesta de la siguiente forma: \n');
-console.log(_); 
+_.words = str =>{
+    return str.split(' ')
+};
+
+_.pad = (str, lng) => {
+    if(str.length >= lng){
+      return str;
+    }
+    const padLength = lng- str.length;
+    let leftPad = Math.floor(padLength/2);
+    let rightPad = padLength-leftPad;
+    while(rightPad > 0){
+      if(leftPad > 0){
+        str = ' ' + str;
+        leftPad--;
+      }
+      str = str + ' ';
+      rightPad--;
+    }
+    return str;
+};
+
+_.has = (obj, key) => {
+    return key in obj;
+}
+
+_.invert = obj =>{
+    let newObj = {};
+    for(let key in obj){
+        newObj[obj[key]] = key;
+    }
+    return newObj;
+}
+
+_.findKey = (obj, predicate) =>{
+    for(let key in obj){
+        if(predicate(obj[key])){
+            return key;
+        }
+    }
+    return undefined;
+};
+
+_.drop = (arr, drop =1) => {
+    return arr.length <= drop? [] : arr.slice(drop);
+  };
+
+_.dropWhile = (arr, predicate) => {
+    let sliceStart = 0;
+    while(predicate(arr[sliceStart], sliceStart, arr)){
+        sliceStart++;
+    };
+    return arr.slice(sliceStart);
+};
+
+_.chunk = (arr, size =1) => {
+    let reArr = [];
+    let sliceStart = 0;
+    if(size === 1){
+        arr.forEach(elem =>{reArr.push([elem])});
+        return reArr;
+    }
+    while(sliceStart < arr.length){
+        reArr.push(arr.slice(sliceStart, size));
+        sliceStart = size;
+        size = size+size;
+    }
+    return reArr;
+};
+
+//console.log('la funcion _ esta compuesta de la siguiente forma: \n');
+//console.log(_); 
 // Do not write or modify code below this line.
 module.exports = _;
